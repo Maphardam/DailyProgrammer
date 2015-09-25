a3b51aaa1a20ea4252e34c5e52722b45d9f0023b
Tim Sabsch <tim@sabsch.com>
2015-09-25 21:58:51 +0200

    Adjusted bash script to handle new structure

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 #author: Tim Sabsch <tim@sabsch.com>
 
 > $1/output.out
-find $1 -type f -name "*.in" | sort | while read inp; do
+find $1/input/ -type f -name "*.in" | sort | while read inp; do
     printf "Output of $inp:\n" >> $1/output.out
     go run $1/solution.go $inp >> $1/output.out
     printf "\n\n" >> $1/output.out
