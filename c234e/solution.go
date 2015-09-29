c727e575ad85bb25d74eee2b7ae3f4f46268a818
Tim Sabsch <tim@sabsch.com>
2015-09-29 15:49:10 +0200

    Correct solution

--- c234e/solution.go
+++ c234e/solution.go
@@ -7,8 +7,9 @@ import (
     "fmt"
     "strconv"
     "strings"
-    "math"
 )
+
+var nv int
 
 func main() {
     file, err := os.Open(os.Args[1])
@@ -21,23 +22,29 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
 	input := strings.Fields(scanner.Text())
-	nv,_ := strconv.Atoi(input[0])
+	nv,_  = strconv.Atoi(input[0])
 	nf,_ := strconv.Atoi(input[1])
 	
-	fmt.Printf("%v %v\n", nv, nf)
-	
-	for f1 := int(math.Pow(10.0, float64(nf-1))); 
-	    f1  < int(math.Pow(10.0, float64(nf))); f1++ {
-	    for f2 := f1; 
-	        f2 < int(math.Pow(10.0, float64(nf+1))); 
-	        f2++ {
-	        if !(f1%10 == 0 && f2%10 == 0) && 
-	            numDigits(f1*f2)== nv && goodDigits(f1,f2){
-	            fmt.Printf("%v=%v*%v\n", f1*f2, f1, f2)
+	mult(1, 0, 10, make([]int,nf))
+    
+}
+
+func mult(v, fi, start int, fs[] int) {
+    for f := start; f <= 99; f++ {
+        fs[fi] = f
+        if fi == len(fs)-1 {
+            if !tooManyZeros(fs) && numDigits(v*f) == nv && 
+                goodDigits(v*f, fs) {
+                fmt.Printf("%v=", v*f)
+                for _,e := range fs[:len(fs)-1] {
+                    fmt.Printf("%v*",e)
+                }
+                fmt.Printf("%v\n",fs[len(fs)-1])
             }
-	    }
-	}
-	
+        } else {
+            mult(v*f, fi+1, f, fs)
+        }
+    }     
 }
 
 func numDigits(n int) (count int){
@@ -48,9 +55,21 @@ func numDigits(n int) (count int){
     return
 }
 
-func goodDigits(f1,f2 int) bool {
+func tooManyZeros(slice []int) bool {
+    oneZero := false
+    for _,el := range slice {
+        if el % 10 == 0 && !oneZero { 
+            oneZero = true
+        } else if el % 10 == 0 && oneZero {
+            return true
+        }
+    }
+    return false
+}
+
+func goodDigits(v int, fs []int) bool {
     // vampire digits
-    v := f1*f2
+
     vd := make([]int, 0)
     for v > 0 {
         vd = append(vd, v%10)
@@ -58,21 +77,14 @@ func goodDigits(f1,f2 int) bool {
     }
     var is bool
     // check fang 1
-    for f1 > 0 {
-        vd, is = inVampire(f1%10, vd)
-        if !is {
-            return false
+    for _,f := range fs {
+        for f > 0 {
+            vd, is = inVampire(f%10, vd)
+            if !is {
+                return false
+            }
+            f /= 10
         }
-        f1 /= 10
-    }
-    
-    // check fang 2
-    for f2 > 0 {
-        vd, is = inVampire(f2%10, vd)
-        if !is {
-            return false
-        }
-        f2 /= 10
     }
     
     return true
